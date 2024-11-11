339a2b98eddd14ec855e0fa7af6663b52bdd8cf3
Mary Shiny Gootam <69759674+maryshinyg@users.noreply.github.com>
2024-11-11 16:10:03 +0530

    (450DSA - Sort) ValEqualToIndex

---
+++ 450 DSA Cracker/Search&Sort/ValEqualToIndex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA45.Search_Sort
+{
+    //https://www.geeksforgeeks.org/problems/value-equal-to-index-value1330/1
+    internal class ValEqualToIndex
+    {
+        public static List<int> valEqualToIndex(List<int> nums)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == i + 1)
+                {
+                    result.Add(i + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
