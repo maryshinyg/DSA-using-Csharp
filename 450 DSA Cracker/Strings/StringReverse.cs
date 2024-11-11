d2f6ccd38b7b4b1a18cb078efb6834b97d3bde57
Mary Shiny Gootam <69759674+maryshinyg@users.noreply.github.com>
2024-11-11 16:13:01 +0530

    (450DSA - String) String Reverse

---
+++ 450 DSA Cracker/Strings/StringReverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA45.Strings
+{
+    internal class StringReverse
+    {
+        public static void ReverseString(char[] s)  //TC: O(N), SC : O(1)
+        {
+            int start = 0;
+            int end = s.Length - 1;
+            char temp;
+            while (start < end)
+            {
+                temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+                start++;
+                end--;
+            }
+            Console.Write("[");
+            for (int i=0; i<s.Length-1; i++)
+            {
+                Console.Write($"\"{(char)s[i]}\",");//(i + ",");
+            }
+            Console.Write($"\"{(char)s[s.Length-1]}\"");
+            Console.Write("]");
+        }
+    }
+}
