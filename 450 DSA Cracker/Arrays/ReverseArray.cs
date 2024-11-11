f5eef30c9fb45f6e7d42450d36c41551acd5f787
Mary Shiny Gootam <69759674+maryshinyg@users.noreply.github.com>
2024-11-11 16:05:23 +0530

    (450DSA) Revere of an array

---
+++ 450 DSA Cracker/Arrays/ReverseArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA45.Arrays
+{
+    internal class ReverseArray
+    {
+        public static int[] reverseArray(int[] arr) //TC: O(N), SC : O(N)
+        {
+            int[] op = new int[arr.Length];
+            for(int i=0; i< arr.Length; i++)
+            {
+                op[i] = arr[arr.Length-i-1];
+            }
+            return op;
+        }
+
+        public static void reverseArrayInPlace(int[] arr) //TC: O(N), SC : O(1)
+        {
+            int start = 0;
+            int end = arr.Length-1;
+            int temp = 0;
+            while (start < end) {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + ",");
+            }
+        }
+    }
+}
